2cc58b2be9444ce388eeaaf16fe41b7a8f3224ad
Pilasande <60289751+Pilasande@users.noreply.github.com>
2023-02-06 20:15:26 +0200

    Add files via upload

---
+++ week_2_homework.sql
@@ -0,0 +1,17 @@
+Question 1
+Answer :  447,770
+
+Question 2
+Answer : 0 5 1 * *
+
+Qestion 3
+Answer : 14,851,920
+
+Question 4
+Answer : 88605
+
+Question 5
+Answer : 514392
+
+Question 6
+Answer : 8
