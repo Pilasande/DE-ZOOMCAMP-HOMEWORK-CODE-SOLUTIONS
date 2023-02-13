26cb7b6ed902a5e7ff90b1ae82aac7a2dd70cdcd
Pilasande <60289751+Pilasande@users.noreply.github.com>
2023-02-13 21:34:40 +0200

    Add files via upload

---
+++ Homework_week3.sql
@@ -0,0 +1,24 @@
+Question 1
+Answer : 43244696
+
+Question 2
+Answer : --rides-bucket02/fhv_data/fhv_tripdata_2019-*.csv.gz
+0 MB for the External Table and 317.94MB for the BQ Table
+
+Question 3
+Answer : 717748
+
+Question 4
+Answer : Partition by pickup_datetime Cluster on affiliated_base_number
+
+Question 5
+Answer : partitioned(This query will process 23.05 MB when run)
+, non partitioned(This query will process 647.87 MB when run.
+Answer :647.87 MB for non-partitioned table and 23.06 MB for the partitioned table
+
+Question 6
+Answer :GCP Bucket
+
+Question 7
+Answer : False
+
